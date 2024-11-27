7a8d17744a451b1528a073fbe240bb7f65c110d0
Priscilla <17732979+priscillaoclark@users.noreply.github.com>
2024-11-27 21:31:13 +0000

    initial next.js app

---
+++ my-app/app/contact/page.tsx
@@ -0,0 +1,10 @@
+// app/contact/page.tsx
+import { Contact1 } from "@/components/contact";
+
+export default function ContactPage() {
+  return (
+    <div className="min-h-screen pt-24">
+      <Contact1 />
+    </div>
+  );
+}
